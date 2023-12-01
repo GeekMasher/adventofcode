8084199f5cf335cc02ddc735b5e53a58809a6387
GeekMasher <geekmasher@gmail.com>
2023-12-01 15:30:48 +0000

    2023(day1): Part 2

--- 2023/src/days/day1.rs
+++ 2023/src/days/day1.rs
@@ -1,6 +1,18 @@
 use crate::Arguments;
 use anyhow::Result;
 
+const MAPPINGS: [(&str, i32); 9] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
 pub fn run(arguments: &Arguments) -> Result<()> {
     let input = super::load_input(arguments)?;
     let mut total = 0;
@@ -8,9 +20,17 @@ pub fn run(arguments: &Arguments) -> Result<()> {
     for line in input.lines() {
         let mut stack: Vec<i32> = Vec::new();
 
-        line.chars()
-            .filter(|chr| chr.is_numeric())
-            .for_each(|chr| stack.push(chr.to_digit(10).unwrap() as i32));
+        for (index, chr) in line.chars().enumerate() {
+            if chr.is_numeric() {
+                stack.push(chr.to_digit(10).unwrap() as i32);
+            } else if let Some(rest_of_line) = line.get(index..) {
+                for (number_string, number) in MAPPINGS {
+                    if rest_of_line.starts_with(number_string) {
+                        stack.push(number);
+                    }
+                }
+            }
+        }
 
         // println!("Stack :: {:?}", stack);
 
